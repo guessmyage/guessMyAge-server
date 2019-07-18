e26d6e264b5351c805cd22e9afc11682e4760cf9
emji-MG <mikelgunawan95@gmail.com>
2019-07-18 16:44:00 +0700

    build up environment

---
+++ app.js
@@ -0,0 +1,29 @@
+if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development'){
+    require('dotenv').config()
+}
+
+const express = require('express')
+const indexRoutes = require('./routes')
+const errorHandler = require('./middlewares/errorHandler')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+mongoose.connect(process.env.MONGODB, {useNewUrlParser:true})
+.then((response) =>{
+    console.log('Connected to Database')
+})
+.catch((err)=>{
+    console.log('Failed connect to Database')
+})
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.use(cors())
+app.use('/', indexRoutes)
+app.use(errorHandler)
+
+app.listen(3000, () =>{
+    console.log('Running on Port :', PORT)
+})
