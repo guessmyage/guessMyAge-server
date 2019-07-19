d7706ee7914786fb38e16f5342babfb88810b4e6
emji-MG <mikelgunawan95@gmail.com>
2019-07-19 13:12:35 +0700

    test alternateAPI

--- routes/recogRoutes.js
+++ routes/recogRoutes.js
@@ -3,5 +3,6 @@ const faceController = require('../controllers/faceController')
 const image = require('../helpers/image.js')
 
 router.post('/', image.multer.single('.....'), image.sendUploadToGCS, faceController.configureAge)
+router.post('/alternate', faceController.alternateAge)
 
 module.exports = router
