4b0d44f1bb21e528ae93146c30b2c8d80b5c5ea3
emji-MG <mikelgunawan95@gmail.com>
2019-07-19 13:12:35 +0700

    test alternateAPI

--- routes/recogRoutes.js
+++ routes/recogRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const faceController = require('../controllers/faceController')
 
 router.post('/', faceController.configureAge)
+router.post('/alternate', faceController.alternateAge)
 
 module.exports = router
